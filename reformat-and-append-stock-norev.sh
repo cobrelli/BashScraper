fa30df5b98162eac3f45bc9e406737aa3e7782ae
Tomi Virtanen <cobrelli@gmail.com>
2013-12-04 19:42:11 +0200

    rev'iton versio stockeista

---
+++ reformat-and-append-stock-norev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export LC_ALL='C'
+
+new=$(./scrape_stock_norev.sh)
+if [ -f stock.txt ];then
+	original=$(cat stock.txt)
+
+	formatted_new=$(echo -e "$new")
+	new_stock="";
+
+	while read -r line;do
+		name=$(echo "$line" |cut -d ',' -f1)
+		new_line=$(grep -- "$name" stock.txt | head -1)
+		new_stock="$new_stock$new_line#$(date +%d.%m.%Y)|$(echo "$line" |cut -d ',' -f2)\n"
+	done <<< "$formatted_new"
+
+	if [ ! -d "backup" ];then
+		echo "backup folder doesn't exist, creating new"
+		mkdir backup
+	fi
+	echo -e "$new_stock" > stock.txt
+	echo -e "$original" > backup/stock_backup_$(date +%d.%m.%Y).txt
+else
+	echo "No existing stock data found, creating new"
+	#formatted_new=$(echo -e "$new" |cut -d ',' -f1)
+	data_format=""
+	while read -r line;do
+		line_start=$(echo "$line" |cut -d ',' -f1)
+		data_format="$data_format$line_start#$(date +%d.%m.%Y)|$(echo "$line" |cut -d ',' -f2)\n"
+	done <<< "$new"
+	echo -e "$data_format" > stock.txt
+fi
---
+++ scrape_stock_norev.sh
@@ -0,0 +1,22 @@
+export LC_ALL='C'
+
+site=$(wget -qO- http://www.kauppalehti.fi/5/i/porssi/porssikurssit/ | grep '<td' | grep 'num\|a href' | gsed -e 's/OMXH/BREAK/g')
+found=0
+new=""
+line_to_append=""
+while read -r line;do
+	if [[ found -eq 0 && "$line" == *\>P*rssi\</td\>* ]]; then
+		found=1
+		line_to_append="$line"
+	elif [[ found -eq 1 ]];then
+		if [[ "$line" == *BREAK* ]];then
+			new="$new $line_to_append\n"
+			line_to_append=""
+			continue;
+		fi
+		line_to_append="$line_to_append $line"
+	fi
+done <<< "$site"
+trim=$(echo -e "$new" | gsed -e 's/<[^>]\+>/,/g' -e 's/^[ ,]*//g' -e 's/,\+/,/g' -e 's/, ,/,/g' -e 's/.*rssi,//g' -e '/^$/d' |cut -d ',' -f1,2|sort)
+echo "$trim"
+
